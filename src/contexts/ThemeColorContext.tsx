ac1ca3684beba84a83152eb850c5a0946480ab36
Your Name <charlesmartins.ads@gmail.com>
2021-06-24 10:37:11 -0300

    created logic for light and dark theme

--- src/contexts/ThemeColorContext.tsx
+++ src/contexts/ThemeColorContext.tsx
@@ -1,9 +1,39 @@
-import { createContext } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react'
 
-type ThemeColor = {   
-   colorTheme: string;
-   changeThemeColor: (theme: string) => void;   
+type Theme = 'light' | 'dark';
+
+type ThemeColorContextProviderProps = {
+  children: ReactNode;
 }
 
-export const ThemeColorContext = createContext({} as ThemeColor);
+type ThemeContextType = {
+  theme: Theme;
+  toggleTheme: () => void;
+}
 
+export const ThemeColorContext = createContext({} as ThemeContextType);
+
+export function ThemeColorContextProvider(props: ThemeColorContextProviderProps) {
+   
+  const [currentTheme, setCurrentTheme] = useState<Theme>(() => {
+    const storagedTheme = localStorage.getItem('theme')
+
+    return (storagedTheme ?? 'light') as Theme;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('theme', currentTheme);
+  }, [currentTheme])
+
+  function toggleTheme() {
+    setCurrentTheme(currentTheme === 'light' ? 'dark' : 'light');
+  }
+
+  return (
+     <div className={currentTheme === 'light' ? 'light-theme' : 'dark-theme'}>
+      <ThemeColorContext.Provider value={{ theme: currentTheme, toggleTheme }}>
+         {props.children}
+      </ThemeColorContext.Provider>
+    </div>
+  )
+}
